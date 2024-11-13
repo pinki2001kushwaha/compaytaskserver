ca1a55a7d7f0ec938777384c3feea619148bf967
shraddha <kushwahapinki@gmail.com>
2024-11-13 15:39:17 +0530

    server side

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.static('public'));
+const PORT =  5000;
 
+
+app.use(cors());
+app.use(express.json());
 
 
 mongoose.connect("mongodb://localhost:27017/project",{
