5a4edf2f16e2e18c1499d9572e1d9dc60cc47f93
shraddha <kushwahapinki@gmail.com>
2024-11-13 15:22:30 +0530

    error

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT =  5000;
-
-
+const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json());
+app.use(express.static('public'));
+
 
 
 mongoose.connect("mongodb://localhost:27017/project",{
